7ff9d32add6511e4c17a3215baa663fc5cfe4e7d
mborssatto <mborssatto@gmail.com>
2022-12-02 14:31:52 +0000

    cleaning database connection file

--- src/database-connection.js
+++ src/database-connection.js
@@ -1,17 +1,3 @@
-// const mongoose = require('mongoose')
-
-// const username = process.env.MONGODB_USERNAME
-// const password = process.env.MONGODB_PASSWORD
-// const dbName = process.env.MONGODB_DATABASE
-
-// mongoose
-//   .connect(`mongodb+srv://${username}:${password}@cluster0.ywoqnd8.mongodb.net/${dbName}?retryWrites=true&w=majority`, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   })
-//   .then(() => console.log('connection established'))
-//   .catch(error => console.log('not connected:', error))
-
 const mongoose = require('mongoose')
 
 // eslint-disable-next-line prefer-const
