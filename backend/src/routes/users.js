12c6025ae19699e3a03ee120a22385ada2505431
mborssatto <mborssatto@gmail.com>
2022-12-23 10:06:46 +0000

    Add email field to users/initialize

--- backend/src/routes/users.js
+++ backend/src/routes/users.js
@@ -13,10 +13,9 @@ let users
 
 /* GET initialize */
 router.get('/initialize', async (req, res) => {
-  const mihri = await User.create({ name: 'mihri', age: 35 })
-  const armagan = await User.create({ name: 'armagan', age: 36 })
-
-  const steve = await User.create({ name: 'steve', age: 21 })
+  const mihri = await User.create({ name: 'mihri', age: 35 , email: 'mihri@mihri.com'})
+  const armagan = await User.create({ name: 'armagan', age: 36, email: 'armagan@arm.com' })
+  const steve = await User.create({ name: 'steve', age: 21, email: 'steve@steve.com' })
   steve.bio = 'An awesome hacker who has seen it all, and now sharing them all with you.'
 
   steve.greet(mihri)
